17eff7d8ee1752e2dd954b8bd85c99a1e2c73936
Jaroslav Jezek <jaroslav.jezek@uhk.cz>
2021-04-18 16:42:28 +0200

    Added SQL scripts for demo data fast insertion. Non deployed app version edited.

--- src/main/java/jobportal/utils/CVExtractor.java
+++ src/main/java/jobportal/utils/CVExtractor.java
@@ -69,16 +69,16 @@ public class CVExtractor {
      */
     public void processCvAndSetTextContentToExtractedTextVariable(MultipartFile[] files) {
         String fileName = files[0].getOriginalFilename();
-        //Path fileNameAndPath = Paths.get("D:\\",fileName);
-        Path fileNameAndPath = Paths.get(new ClassPathResource("filename").getPath());
+        Path fileNameAndPath = Paths.get("D:\\",fileName);
+        // - Heroku deployed version --> Path fileNameAndPath = Paths.get(new ClassPathResource("filename").getPath());
         try {
             Files.write(fileNameAndPath, files[0].getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //File savedFile = new File("D:\\" + files[0].getOriginalFilename());
-        File savedFile = new File(String.valueOf(fileNameAndPath));
+        File savedFile = new File("D:\\" + files[0].getOriginalFilename());
+        // - Heroku deployed version --> File savedFile = new File(String.valueOf(fileNameAndPath));
 
         // Extracting of whole text content from attached PDF / DOCX file
         this.extractedText = getCvTextData(savedFile, fileName);
